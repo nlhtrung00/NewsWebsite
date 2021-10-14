23fdad525635bb76241cf4eac592cd3d3b40b77e
trung <nlhtrung120200@gmail.com>
2021-10-14 10:46:00 +0700

    add cors again

--- TTNewsBE/TTNewsBE/Startup.cs
+++ TTNewsBE/TTNewsBE/Startup.cs
@@ -52,6 +52,8 @@ namespace TTNewsBE
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.WithOrigins("https://localhost:3000")
+            .AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
